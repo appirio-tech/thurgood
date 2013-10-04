0b37d8f9b3f9ee96950b09c8c550ad671f1e35ab
Aluxian <rosian_alex@yahoo.com>
2013-10-04 17:29:07 +0300

    #3: Implemented /servers - POST

--- actions/servers.js
+++ actions/servers.js
@@ -1,9 +1,11 @@
+var _ = require('underscore');
+
 exports.action = {
   name: "serversCreate",
   description: "Creates a new server. Method: POST",
   inputs: {
     required: [],
-    optional: [],
+    optional: ['name', 'status', 'instanceUrl', 'operatingSystem', 'installedServices', 'languages', 'platform', 'repoName', 'username', 'password', 'jobId'],
   },
   authenticated: false,
   outputExample: {},
@@ -11,7 +13,15 @@ exports.action = {
   run: function(api, connection, next) {
     var serversCollection = api.mongo.collections.servers;
     var serverDoc = api.mongo.schema.new(api.mongo.schema.server);
-    
+
+    // Assign parameters
+    _.each(serverDoc, function(value, key) {
+      if (connection.params[key]) {
+        serverDoc[key] = connection.params[key];
+      }
+    });
+
+    // Insert document
     serversCollection.insert(serverDoc, {w:1}, function(err, result) {
       if (!err) {
         connection.rawConnection.responseHttpCode = 201;
