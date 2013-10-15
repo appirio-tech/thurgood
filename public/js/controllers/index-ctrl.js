6219da57fdffd95eb8be90a078db6207d806d095
commfort <comfort@gmail.com>
2013-10-15 04:37:45 +0300

    cs2953 submission
    Solution for Angular Face for Thurgood's MEAN Stack
    http://www.cloudspokes.com/challenges/2953

---
+++ public/js/controllers/index-ctrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+ThurgoodApp.controller('IndexCtrl',
+	['$scope', '$location', '$routeParams', '$timeout', '$http', function ($scope, $location, $routeParams, $timeout, $http) {
+
+	}]);
