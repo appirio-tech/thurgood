732edd0124ef2debaf4b7e6c1d1be6bbd91b9a3b
Jeff Douglas <jeff@jeffdouglas.com>
2013-10-18 09:02:06 -0400

    removed trailing space from route

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ exports.routes = {
     { path: "/:apiVersion/jobs/:id/complete", action: "jobsComplete" },
     { path: "/:apiVersion/jobs/:id", action: "jobsFetch" },
     { path: "/:apiVersion/jobs", action: "jobsFetch" },
-    { path: "/:apiVersion/pt/token/:key ", action: "ptFetchToken" }
+    { path: "/:apiVersion/pt/token/:key", action: "ptFetchToken" }
   ],
 
   post: [
