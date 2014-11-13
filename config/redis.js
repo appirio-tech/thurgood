6261e2dea2a422e3dc874376994e21ec37b500f2
Rohit Manethiya <rohitand07@gmail.com>
2014-11-13 11:37:47 +0530

    Using redis instead of fakeredis

--- config/redis.js
+++ config/redis.js
@@ -11,9 +11,9 @@ exports.default = {
       // How long to wait for an RPC call before considering it a failure 
       rpcTimeout: 5000, 
 
-      package: 'fakeredis',
+      //package: 'fakeredis',
 
-      // package: 'redis',
+      package: 'redis',
       // host: '127.0.0.1',
       // port: 6379,
       // password: null,
